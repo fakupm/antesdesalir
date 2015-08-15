c3baf286162d19d19fccdeb9a7df6b1522440847
fakupm <fakulong@gmail.com>
2015-08-15 17:28:11 -0300

    hola
    cambio de diseño

--- www/js/app.js
+++ www/js/app.js
@@ -26,7 +26,7 @@ angular.module('starter', ['ionic'])
     }
   });
 })
-
+/*
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
@@ -95,9 +95,59 @@ angular.module('starter', ['ionic'])
       }
     }
   })
-  
+
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/clima');
 
+})
+*/
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.clima', {
+    url: '/clima',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/clima.html'
+      }
+    }
+  })
+  .state('app.subte', {
+      url: '/subte',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/subte.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
 });
