31fdf10a2d4e158f375de2ad3a14fd48373be1cb
fakupm <fakulong@gmail.com>
2015-08-18 23:36:02 -0300

    hola
    mapa del subte

--- www/js/app.js
+++ www/js/app.js
@@ -13,20 +13,6 @@ angular.module('starter', ['ionic'])
   });
 })
 
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-/*
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
@@ -100,55 +86,5 @@ angular.module('starter', ['ionic'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/clima');
 
-})
-
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.clima', {
-    url: '/clima',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/clima.html'
-      }
-    }
-  })
-  .state('app.subte', {
-      url: '/subte',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/subte.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
 });
 */
